6c5008b6fa3e8040c5d9173db1ef90c24d52bd93
NoelanL <92019519+NoelanL@users.noreply.github.com>
2021-11-10 11:23:17 +0100

    ..

---
+++ app.js
@@ -0,0 +1,9 @@
+let result = 5 + 6;
+let addition = (result + 2);
+let soustraction = (result - 1);
+let modulo = 5 % 2;
+
+alert(`${result} * ${addition}`);
+alert(`${result} ** ${soustraction}`);
+
+console.log(modulo);
