63df5b9f5edf93adb8de3b982e95aada38692816
NoelanL <92019519+NoelanL@users.noreply.github.com>
2021-11-10 11:55:01 +0100

    rectification du modulo

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ let addition = (result + 2);
 let soustraction = (result - 1);
 let modulo = 5 % 2;
 
-alert(`${result} * ${addition}`);
-alert(`${result} ** ${soustraction}`);
-
-console.log(modulo);
+alert(`${result * addition}`);
+alert(`${result % soustraction}`);
+alert(`${modulo}`)
